cd89b8937f1392fd76ec6f668a0ccbce4f2afee0
yusuparipin30 <yusuparipin30@gmail.com>
2022-12-13 00:21:46 +0700

    from add user

---
+++ src/components/MemberList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const MemberList = () => {
+    return (
+        <div>
+        <h1 className='title'>Member</h1>
+      <h2 className='subtitle'>List Of Member</h2>
+      <table className="table is-striped is-fullwidth">
+          <thead>
+              <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Marital_sts</th>
+                  <th>Category_people</th>
+                  <th>Active_state</th>
+                  <th>Created By</th>
+                  <th>Actions</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+    );
+};
+
+
+export default MemberList
---
+++ src/pages/Member.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./Layout";
+import MemberList from "../components/MemberList";
+
+const Member = () => {
+    return (
+        <Layout>
+            <MemberList/>
+        </Layout>
+    );
+};
+
+export default Member
